7bfba5f96702d86a2f05df5964cce8105584baf3
Estela Ramirez <ramirezestela01@gmail.com>
2022-04-27 22:51:16 -0700

    made node class more general

--- node.py
+++ node.py
@@ -1,20 +1,16 @@
-import re
-import numpy as np
-
-
 """
 Node class has a state (puzzle state), (pointer to) parent, and children attributes
 The children attribute is a list that will hold pointers to child nodes
-empty space is represented with 9
+empty space is represented with 0
 """
 
 class Node:
-    def __init__(self, state = [1, 2, 3, 4, 5, 6, 7, 8, 9]):
+    def __init__(self, state = [1, 2, 3, 4, 5, 6, 7, 8, 0]):
         self.state = state  # state of puzzle 
         self.parent = None
         self.children = []
-        self.size = 3       # num of rows, cols for puzzle
-
+        self.cost = 1
+        
     def get_state(self):
         return self.state
 
@@ -24,43 +20,14 @@ class Node:
     def get_children(self):
         return self.children
 
+    def set_parent(self, parent):
+        self.parent = parent
 
-    def is_leaf_node(self):
-        if len(self.children) == 0:
-            return True
-        return False
+    def get_cost(self):
+        return self.cost
 
-    def print_state(self):
-        for i in range(self.state):
-            if i % self.size == 0:
-                print()
-            else:
-                pass
-            print(self.state[i])
+    def add_child(self, child):
+        self.children.append(child)
 
-    # return True if Node has goal state, False otherwise
-    def is_goal_state(self):
-        val = self.state[0]
-        for x in range(1, len(self.state)):
-            if val > self.state[x]:
-                return False
-            val = self.state[x]
-        return True
-
-    def get_empty_space_loc(self):
-        for i in range(len(self.state)):
-            if self.state[i] == 9:
-                return i 
-        return 0
-
-    def move_right(self):
-        pass
-
-    def move_left(self):
-        pass
-
-    def move_up(self):
-        pass
-
-    def move_down(self):
-        pass  
+    def set_cost(self, cost):
+        self.cost = cost
